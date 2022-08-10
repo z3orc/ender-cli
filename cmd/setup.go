06661694c8e11bdbab99d4959a98bc4f444885e4
z3orc <z3orc@users.noreply.github.com>
2022-08-10 02:27:28 +0200

    Initial setup command

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,10 +1,13 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/spf13/cobra"
+	"github.com/z3orc/ender-cli/global"
 )
 
 // setupCmd represents the setup command
@@ -37,4 +40,48 @@ func init() {
 }
 
 func setup(){
+	dir_res := create_directories()
+	if dir_res != 0 {
+		log.Fatal("Could not create directories")
+	}
+
+	dl_res  := download_file(global.JAR_PATH, "http://dynamic.z3orc.com/paper/1.19")
+	if dl_res != nil {
+		log.Fatal("Could not download jar file")
+	}
+}
+
+func create_directories() int{
+
+	dirs := [4]string{global.BACKUP_DIR, global.BIN_DIR, global.CONFIG_DIR, global.DATA_DIR}
+
+	for _, dir := range dirs {
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			return 1
+		}
+	}
+	return 0
+
+}
+
+func download_file(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
