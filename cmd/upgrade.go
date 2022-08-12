14661cab3007e6132c16e59a17e7971f443b1989
z3orc <z3orc@users.noreply.github.com>
2022-08-12 23:05:54 +0200

    Add upgrade command

--- cmd/upgrade.go
+++ cmd/upgrade.go
@@ -5,9 +5,19 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
+	"github.com/briandowns/spinner"
+	"github.com/erikgeiser/promptkit/selection"
 	"github.com/spf13/cobra"
-	"github.com/z3orc/ender-cli/endpoints"
+	"github.com/z3orc/ender-cli/config"
+	"github.com/z3orc/ender-cli/endpoints/paper"
+	"github.com/z3orc/ender-cli/endpoints/purpur"
+	"github.com/z3orc/ender-cli/endpoints/vanilla"
+	"github.com/z3orc/ender-cli/global"
+	"github.com/z3orc/ender-cli/util"
 )
 
 // upgradeCmd represents the upgrade command
@@ -40,12 +50,94 @@ func init() {
 }
 
 func upgrade(){
-	possible_versions()
+	current_version := config.Get(global.CONFIG_ENDER_PATH, "version")
+	flavour := config.Get(global.CONFIG_ENDER_PATH, "flavour")
+	choices := possible_versions(current_version, flavour, false)
+
+	if len(choices) == 0 {
+		fmt.Println("🚀 You are already on the latest version!")
+		os.Exit(0)
+	}
+
+	sp := selection.New("Choose new server version",
+	selection.Choices(choices))
+	sp.PageSize = 6
+
+	new_version, err := sp.RunPrompt()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+
+		os.Exit(1)
+	}
+
+	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
+	s.Suffix = " Upgrading server"
+	s.FinalMSG = "✅ Server upgraded!"
+	spinnerErrorMsg := "Could not upgrade server! \n"
+	s.Start()
+	time.Sleep(1 * time.Second)
+
+	err = config.Set(global.CONFIG_ENDER_PATH, "version", new_version.String)
+	if err != nil {
+		s.FinalMSG = spinnerErrorMsg
+		s.Stop()
+		log.Fatalln(err)
+	}
+
+	err = util.DownloadFile(global.JAR_PATH, "https://dynamic.z3orc.com/" + flavour + "/" + new_version.String)
+	if err != nil {
+		s.FinalMSG = spinnerErrorMsg
+		s.Stop()
+		config.Set(global.CONFIG_ENDER_PATH, "version", current_version)
+		log.Fatalln(err)
+	}
+
+	s.Stop()
+	
 }
 
-func possible_versions(){
-	// var possible_versions []string;
+func possible_versions(current_version string, flavour string, snapshot bool) []string{
+	var possible_versions []string;
 
-	versions := endpoints.GetVersionsVanilla()
-	fmt.Println(versions.Versions[0])
+	if flavour == "vanilla" {
+		versions := vanilla.GetVersions().Versions
+
+		for _, version := range versions {
+			if version.Type != "snapshot" {
+				if version.Id == current_version {
+					break
+				} else {
+					possible_versions = append(possible_versions, version.Id)
+				}
+			}
+		}
+	} else if flavour == "paper" {
+		versions := paper.GetVersions().Versions
+		for i, j := 0, len(versions)-1; i < j; i, j = i+1, j-1 {
+			versions[i], versions[j] = versions[j], versions[i]
+		}
+
+		for index := range versions {
+			if versions[index] == current_version {
+				break
+			} else {
+				possible_versions = append(possible_versions, versions[index])
+			}
+		}
+	} else if flavour == "purpur" {
+		versions := purpur.GetVersions().Versions
+		for i, j := 0, len(versions)-1; i < j; i, j = i+1, j-1 {
+			versions[i], versions[j] = versions[j], versions[i]
+		}
+
+		for index := range versions {
+			if versions[index] == current_version {
+				break
+			} else {
+				possible_versions = append(possible_versions, versions[index])
+			}
+		}
+	}
+
+	return possible_versions
 }
