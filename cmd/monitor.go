aa6fd384e2dc09aa10f85b38cedaa290547d5609
z3orc <z3orc@users.noreply.github.com>
2022-08-20 00:52:08 +0200

    Add description

--- cmd/monitor.go
+++ cmd/monitor.go
@@ -18,13 +18,13 @@ import (
 // monitorCmd represents the monitor command
 var monitorCmd = &cobra.Command{
 	Use:   "monitor",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Starts the server and opens ipc.",
+// 	Long: `A longer description that spans multiple lines and likely contains examples
+// and usage of using your command. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+// Cobra is a CLI library for Go that empowers applications.
+// This application is a tool to generate the needed files
+// to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		monitor()
 	},
--- cmd/root.go
+++ cmd/root.go
@@ -6,21 +6,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "Ender CLI",
+	Use:   "ender-cli",
 	Short: "A cli application to setup and manage a Minecraft-server",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Long: `A cli application to setup and manage a Minecraft-server`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
