45810cfff852ac410451deab32aef3e6580e5e3e
z3orc <z3orc@users.noreply.github.com>
2023-06-26 15:18:26 +0200

    use cli and parser

--- main.go
+++ main.go
@@ -1,30 +1,18 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/z3orc/ender-cli/backup"
-	"github.com/z3orc/ender-cli/logger"
+	"github.com/z3orc/ender-cli/commands"
+	cli "github.com/z3orc/simple-cli"
 )
 
 func main() {
-	// root := cli.New(cli.Cli{
-	// 	Name:        "ender",
-	// 	Description: "A command line tool for managing a Minecraft server.",
-	// 	Usage:       "ender <command> [args...]",
-	// 	Commands:    commands.All(),
-	// 	Debug:       false,
-	// })
+	root := cli.New(cli.Cli{
+		Name:        "ender",
+		Description: "A command line tool for managing a Minecraft server.",
+		Usage:       "ender <command> [args...]",
+		Commands:    commands.All(),
+		Debug:       false,
+	})
 
-	// root.Parser()
-
-	backup, err := backup.New()
-	if err != nil {
-		logger.Error.Println(err)
-	}
-
-	fmt.Println(backup)
-
-	// result := console.ParseToLogLine("[17:37:54 INFO]: Building unoptimized datafixer")
-	// fmt.Println(result)
+	root.Parser()
 }
