9fdc3adfa0125184ec380d000e8ea37f0f6434d1
z3orc <z3orc@users.noreply.github.com>
2023-06-26 12:27:46 +0200

    add backup command

--- commands/root.go
+++ commands/root.go
@@ -5,7 +5,7 @@ import cli "github.com/z3orc/simple-cli"
 var all []*cli.Command
 
 func init() {
-	all = []*cli.Command{Start}
+	all = []*cli.Command{Start, Backup}
 }
 
 func All() []*cli.Command {
